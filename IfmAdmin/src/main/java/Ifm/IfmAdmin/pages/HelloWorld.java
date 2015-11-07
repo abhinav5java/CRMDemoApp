3965c5461dd288978b11293e40b2b593165adda8
Abhinav <abhinav5java@gmail.com>
2015-11-08 00:08:16 +0530

    hfg

---
+++ IfmAdmin/src/main/java/Ifm/IfmAdmin/pages/HelloWorld.java
@@ -0,0 +1,11 @@
+package Ifm.IfmAdmin.pages;
+
+/** A page class (automatically associated with the template file of the same name). */
+public class HelloWorld {
+    
+    /** An ordinary getter. */ 
+    public String getUsername() {
+        return "world";
+    }
+    
+}
